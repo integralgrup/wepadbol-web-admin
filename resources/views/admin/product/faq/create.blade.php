82e573833b8b0b73a91099ddfe48c52683c56a8a
yemliha81 <yemlihaddln@hotmail.com>
2025-11-17 16:07:36 +0300

    Faq club fix 3

--- resources/views/admin/product/faq/create.blade.php
+++ resources/views/admin/product/faq/create.blade.php
@@ -52,7 +52,7 @@
                         </ul>
                     </div>
                     <div class="card-body">
-                        <form action="{{ route('admin.product.faq.store', $product->id) }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('admin.product.faq.store', $product->product_id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="tab-content" id="myTabContent">
                                 @foreach($languages as $language)
