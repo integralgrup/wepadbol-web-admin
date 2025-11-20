52ac23e0c0e711f6160001b2f05384dd5bb85739
yemliha81 <yemlihaddln@hotmail.com>
2025-11-20 09:20:50 +0300

    Robots noindex added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,6 +8,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- robots noindex, npfollow -->
+    <meta name="robots" content="noindex, nofollow">
+
     @if(isset($seo))
     <meta name="keywords" content="{{ $seo->seo_keywords }}">
     <meta name="description" content="{{ $seo->seo_description }}">
