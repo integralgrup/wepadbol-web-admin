4748855a66fc7db0c30cd41dc5fcd1b1d303ce6f
yemliha81 <yemlihaddln@hotmail.com>
2026-01-28 17:02:20 +0300

    Product create fix 3

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -563,6 +563,7 @@ class ProductController extends Controller
     public function categoryCreate()
     {
         $categories = ProductCategory::where('lang', app()->getLocale())->get();
+        dd($categories);
         return view('admin.product.category.create', compact('categories'));
     }
     public function categoryStore(Request $request)
